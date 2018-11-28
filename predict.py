2bab05512822a1503329a4645a8ce71264a2c3f0
HgN37 <hgnguyen37@gmail.com>
2018-11-28 20:34:05 +0700

    "Add human reading label"

--- predict.py
+++ predict.py
@@ -4,10 +4,14 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras.optimizers import Adam
 import numpy as np
+import os
+import time
 
 img_width, img_height = 128, 128
 img_size = (img_width, img_height, 1)
 batch_size = 50
+
+print('Importing model...')
 
 model = Sequential()
 
@@ -37,13 +41,30 @@ model.add( Activation('softmax') )
 
 model.load_weights('./model/model_v10.h5')
 
+print('Model imported')
+
+print('Importing class list')
+
+with open('./model/class_list_v10.txt') as f:
+    class_list = f.readlines()
+class_list = [x.strip() for x in class_list]
+
+print('Class list imported')
+
 predict_img = ImageDataGenerator();
-predict_gen = predict_img.flow_from_directory(
-    './test/',
-    target_size=(img_width,img_height),
-    color_mode='grayscale',
-    batch_size=batch_size,
-    class_mode='sparse'
-)
-x = model.predict_generator(predict_gen, steps=1)
-print(x)
+while True:
+    input('Press to test')
+    os.system('clear')
+    print(class_list)
+    predict_gen = predict_img.flow_from_directory(
+        './test/',
+        target_size=(img_width,img_height),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        class_mode='sparse'
+    )
+    output_raw = model.predict_generator(predict_gen, steps=1, verbose=0)
+    onehot = output_raw.tolist()
+    print()
+    print(class_list[onehot[0].index(1)])
+    print()
